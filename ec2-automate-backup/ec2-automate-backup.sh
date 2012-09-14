e69fda405d613ed5de0e0b8a22561ad5f7b1a733
Colin Johnson <colinbjohnson@gmail.com>
2012-09-14 23:59:56 +0000

    add enhancement - ec2-automate-backup.sh now writes tag Name=value if requested and handles ec2-create-snapshot errors.

--- ec2-automate-backup/ec2-automate-backup.sh
+++ ec2-automate-backup/ec2-automate-backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -
 # Author: Colin Johnson / colin@cloudavail.com
-# Date: 2012-09-07
+# Date: 2012-09-14
 # Version 0.1
 # License Type: GNU GENERAL PUBLIC LICENSE, Version 3
 #
@@ -40,7 +40,7 @@ get_EBS_list()
 	#takes the output of the previous command 
 	ebs_backup_list_result=`echo $?`
 	if [[ $ebs_backup_list_result -gt 0 ]]
-		then echo -e "An error occured when running ec2-describe-volumes. The error thrown is below:\n$ebs_backup_list_complete" 1>&2 ; exit 64
+		then echo -e "An error occured when running ec2-describe-volumes. The error returned is below:\n$ebs_backup_list_complete" 1>&2 ; exit 70
 	fi
 	ebs_backup_list=`echo "$ebs_backup_list_complete" | grep ^VOLUME | cut -f 2`
 	#code to right will output list of EBS volumes to be backed up: echo -e "Now outputting ebs_backup_list:\n$ebs_backup_list"
@@ -56,15 +56,18 @@ selection_method="volumeid"
 region="us-east-1"
 #sets date variable
 date_current=`date -u +%Y-%m-%d`
+#sets the "Name" tag set for a snapshot to false - using "Name" requires that ec2-create-tags be called in addition to ec2-create-snapshot
+name_tag_set=false
 
 #handles options processing
-while getopts :s:r:v:t: opt
+while getopts :s:r:v:t:n opt
 	do
 		case $opt in
 			s) selection_method="$OPTARG";;
 			r) region="$OPTARG";;
 			v) volumeid="$OPTARG";;
 			t) tag="$OPTARG";;
+			n) name_tag_set=true;;
 			*) echo "Error with Options Input. Cause of failure is most likely that an unsupported parameter was passed or a parameter was passed without a corresponding option." 1>&2 ; exit 64;;
 		esac
 	done
@@ -75,5 +78,13 @@ get_EBS_list
 #the loop below is called once for each volume in $ebs_backup_list - the currently selected EBS volume is passed in as "ebs_selected"
 for ebs_selected in $ebs_backup_list
 do
-	ec2-create-snapshot --region $region -d ${ebs_selected}_$date_current $ebs_selected
+	ec2_snapshot_description="ec2ab_${ebs_selected}_$date_current"
+	ec2_create_snapshot_result=`ec2-create-snapshot --region $region -d $ec2_snapshot_description $ebs_selected 2>&1`
+	if [[ $? != 0 ]]
+		then echo -e "An error occured when running ec2-create-snapshot. The error returned is below:\n$ec2_create_snapshot_result" 1>&2 ; exit 70
+	elif $name_tag_set
+		then
+		ec2_snapshot_resource_id=`echo "$ec2_create_snapshot_result" | cut -f 2`
+		ec2-create-tags $ec2_snapshot_resource_id --region $region --tag Name=$ec2_snapshot_description
+	fi
 done
