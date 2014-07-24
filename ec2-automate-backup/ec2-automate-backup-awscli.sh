45d2b10c7b3702101e789518140b486b940e4871
kquinsland <karl+github@karlquinsland.com>
2014-07-24 12:13:57 -0700

    Update ec2-automate-backup-awscli.sh
    small typo fix

--- ec2-automate-backup/ec2-automate-backup-awscli.sh
+++ ec2-automate-backup/ec2-automate-backup-awscli.sh
@@ -205,7 +205,7 @@ for ebs_selected in $ebs_backup_list; do
   ec2_snapshot_description="ec2ab_${ebs_selected}_$current_date"
   ec2_create_snapshot_result=$(aws ec2 create-snapshot --region $region --description $ec2_snapshot_description --volume-id $ebs_selected --output text 2>&1)
   if [[ $? != 0 ]]; then
-    echo -e "An error occured when running ec2-create-snapshot. The error returned is below:\n$ec2_create_snapshot_result" 1>&2 ; exit 70
+    echo -e "An error occurred when running ec2-create-snapshot. The error returned is below:\n$ec2_create_snapshot_result" 1>&2 ; exit 70
   else
     ec2_snapshot_resource_id=$(echo "$ec2_create_snapshot_result" | cut -f 5)
   fi  
